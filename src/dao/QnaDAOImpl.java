4d74b64ae72b4ab6fa9a19fc75e97eb2786344ad
skdksmsqns <skdksmsqns@gmail.com>
2019-08-07 18:02:02 +0900

    ddd

---
+++ src/dao/QnaDAOImpl.java
@@ -0,0 +1,57 @@
+package dao;
+
+import java.util.List;
+
+import model.Qna;
+
+public class QnaDAOImpl extends BaseDAO implements QnaDAO  {
+
+	@Override
+	public Qna QnaInsert(Qna qna) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Qna QnaComment(Qna qna) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Qna> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Qna seleteByNo(int no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Qna qna) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int qna_no) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateVisited(int qna_no) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Qna> selectAllPage(int rowStartNumber, int rowEndNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
--- src/model/Payment.java
+++ src/model/Payment.java
@@ -2,7 +2,7 @@ package model;
 
 public class Payment {
 
-	int payment_no1;
+	int payment_no;
 	int lecture_no;
 	int member_no;
 	String id;
